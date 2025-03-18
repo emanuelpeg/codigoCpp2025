9b9b7d3328617b6be1cee4462022436096d75f38
Emanuel <emanuelpeg@yahoo.com.ar>
2025-03-17 23:58:39 -0300

    clase 2

--- clase2/factorial/factorial/main.cpp
+++ clase2/factorial/factorial/main.cpp
@@ -7,15 +7,72 @@ long factorial(int n) {
     return n * factorial(n - 1);
 }
 
+long factorialImp(int n) {
+    long result = 1;
+    int cont = 1;
+    while (cont <= n) {
+        result *= cont;
+        cont++;
+    }
+    return result;
+}
 
-long factorialTail(int n, int count, long factoriaAnt) {
-    if (n < count) return factoriaAnt;
-    return factorialTail(n, count + 1, count * factoriaAnt);
+long factorialTailAux(int n, int cont, long fa) {
+    if (cont == n) return n * fa;
+    return factorialTailAux(n, cont + 1, cont * fa);
+}
+
+long factorialTail(int n) {
+    if (n == 0) return 1;
+    return factorialTailAux(n,1,1);
+}
+
+long fibo(int n) {
+    if (n == 0 || n==1) return 1;
+    return fibo(n-1) + fibo(n-2);
+}
+
+long fiboTailAux(int n, int cont, long fa, long faa) {
+    if (n == cont) return fa;
+    return fiboTailAux(n, cont + 1, fa + faa, fa);
+}
+
+long fiboTail(int n) {
+    if (n == 0 || n==1) return 1;
+    return fiboTailAux(n, 0, 1, 0);
+}
+
+long mcd(long num, long den) {
+    if ((num % den) == 0) return den;
+    return mcd(den, num % den);
+}
+
+long acuAux(int vec[], int n, long acu) {
+    if (n == 1) return vec[0] + acu;
+    return acuAux(vec, n - 1, acu + vec[n-1]);
+}
+
+long acu(int vec[], int n) {
+    return acuAux(vec, n , 0);
+}
+
+long acu2(int vec[], int n, int cont) {
+    if (n == cont) return 0;
+    return vec[cont] + acu2(vec, n, cont + 1);
+}
+
+int max(int vec[], int n) {
+    if (n == 1) return vec[0];
+    int maximo = max(vec, n -1);
+    if (vec[n-1] > maximo) return vec[n-1];
+    return maximo;
 }
 
 int main()
 {
-    cout << factorial(5)<< endl;
-    cout << factorialTail(5,1,1)<< endl;
+    int vec[] = {10,2,4,20,1};
+    //cout << acu(vec,5)<< endl;
+    //cout << acu2(vec,5, 0)<< endl;
+    cout << max(vec,5)<< endl;
     return 0;
 }
